f8bf5dda6cdc2e350d286591ab1265fb818d94b8
Robert Lönnqvist <robert.lonnqvist@cygni.se>
2020-07-15 11:40:49 +0200

    Include vimrc in installation

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -sf ${PWD}/bash_profile ${HOME}/.bash_profile
 ln -sf ${PWD}/bashrc ${HOME}/.bashrc
 ln -sf ${PWD}/zshrc ${HOME}/.zshrc
 ln -sf ${PWD}/gitconfig ${HOME}/.gitconfig
+ln -sf ${PWD}/vimrc ${HOME}/.vimrc
