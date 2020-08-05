7b702920f8b31559c1dbc4da77b547065026bd01
Robert Lönnqvist <robert.lonnqvist@cygni.se>
2020-08-05 20:50:59 +0200

    Fix shellcheck warnings

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
-ln -sf ${PWD}/inputrc ${HOME}/.inputrc
-ln -sf ${PWD}/bash_profile ${HOME}/.bash_profile
-ln -sf ${PWD}/bashrc ${HOME}/.bashrc
-ln -sf ${PWD}/zshrc ${HOME}/.zshrc
-ln -sf ${PWD}/gitconfig ${HOME}/.gitconfig
-ln -sf ${PWD}/vimrc ${HOME}/.vimrc
+ln -sf "${PWD}/inputrc" ~/.inputrc
+ln -sf "${PWD}/bash_profile" ~/.bash_profile
+ln -sf "${PWD}/bashrc" ~/.bashrc
+ln -sf "${PWD}/zshrc" ~/.zshrc
+ln -sf "${PWD}/gitconfig" ~/.gitconfig
+ln -sf "${PWD}/vimrc" ~/.vimrc
