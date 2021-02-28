11a53328078bff161cdb7d7885cd5fc51b3f45cd
Robert Lönnqvist <robert.lonnqvist@gmail.com>
2021-02-28 11:06:10 +0100

    Add back vimrc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ln -sf "${PWD}/inputrc" ~/.inputrc
-ln -sf "${PWD}/bash_profile" ~/.bash_profile
-ln -sf "${PWD}/bashrc" ~/.bashrc
-ln -sf "${PWD}/zshrc" ~/.zshrc
-ln -sf "${PWD}/gitconfig" ~/.gitconfig
+for file in inputrc bash_profile bashrc zshrc gitconfig vimrc
+do
+  ln -sf "${PWD}/${file}" "${HOME}/.${file}"
+done
+unset file
